b60c2331d80c6acd1eaea496d8d032c572ca3366
Yohannes Guesh <yohannesguesh01@gmail.com>
2024-01-21 11:14:34 -0800

    updated

--- sort.h
+++ sort.h
@@ -1,10 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-
 #include <stdio.h>
 #include <stdlib.h>
-
-
 /**
  * enum bool - Enumeration of Boolean values.
  * @false: Equals 0.
@@ -25,9 +22,9 @@ typedef enum bool
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
